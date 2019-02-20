afe7b34dfcb208f4131f50cb27753ee2abe21dfa
Vibhoothi <vibhoothiiaanand@gmail.com>
2019-02-20 21:46:05 +0530

    main: Take the input and print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Simple Game of Guessing number written in rust");
+    println!("Enter the number");
+    /* Create a variable /guess/ which is mutable for changing it dynamically which is empty string. 
+     * The string argument needs to be mutable so the method can change the string’s content by adding the user input.
+    */
+    let mut guess = String::new();
+    /* Input as stdin(std::io::stdin) called readline into std input and variable guess 
+     * .expect("Failed to read line"); handles error when wrong input
+     */
+    io::stdin().read_line(&mut guess).expect("Failed to read Line");
+    println!("You guessed: {}", guess);
+
 }
