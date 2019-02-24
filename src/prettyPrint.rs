d13c8d286240174b840d2cf0b3db0a0e168eb70d
Vibhoothi <vibhoothiiaanand@gmail.com>
2019-02-24 23:39:16 +0530

    prettyPrint: A basic program with diffrent type of print statements

---
+++ src/prettyPrint.rs
@@ -0,0 +1,15 @@
+fn main()
+{
+  // {} is replaced of %d like that when comapring to C/C++
+   println!("{} days in a month", 31);
+  // Assign numbers
+   println!("{1}, this is {1}. {1}, this is {0}","Alic","Bob");
+ // add name to the things
+   println!("{subject} {verb} {object}", object = "the lazy dog", subject = "the fox", verb ="jumps over"); 
+ // :b denote binary
+   println!("{} of {:b} people know binary, the other half doesn't",1,2);
+ // {:?} is similar with {}
+    println!("{:?} months in a year.", 12);  
+
+}
+
